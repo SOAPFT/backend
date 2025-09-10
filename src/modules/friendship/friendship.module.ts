4a52bd7c8ae4205631c425a1f9d0917f3b032f07
Jaewoo Kim <164657817+nullisdefined@users.noreply.github.com>
2025-09-10 18:05:56 +0900

    fix: dependency injection

--- src/modules/friendship/friendship.module.ts
+++ src/modules/friendship/friendship.module.ts
@@ -4,9 +4,13 @@ import { FriendshipController } from './friendship.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Friendship } from '@/entities/friendship.entity';
 import { User } from '@/entities/user.entity';
+import { NotificationsModule } from '@/modules/notifications/notifications.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Friendship, User])],
+  imports: [
+    TypeOrmModule.forFeature([Friendship, User]),
+    NotificationsModule,
+  ],
   controllers: [FriendshipController],
   providers: [FriendshipService],
   exports: [FriendshipService],
